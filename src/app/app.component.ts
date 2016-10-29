00db65f5cbe3c42dd4c94fda0d88d225deb35f2a
Vasin Max Limsukhawat <max.limsukhawat@qsc.com>
2016-10-28 21:51:35 -0600

    working firebase with no auth...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseApp } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works!as';
+
+  items: FirebaseListObservable<any[]>;
+
+  image: string;
+  storageRef;
+  constructor(af: AngularFire, @Inject(FirebaseApp) firebaseApp: any) {
+    this.items = af.database.list('items');
+    // this.items.push({name: 'something4'});
+    // this.items.update('something', {name: 'somethingchanged'});
+    // this.items.remove('something');
+
+
+    this.storageRef = firebaseApp.storage().ref();
+
+    this.storageRef.child('jjda-somethignelse.jpg').getDownloadURL().then((url) => {
+        this.image = url
+        console.log(url);
+      });
+  }
+
+  submit(@Inject(FirebaseApp) firebaseApp: any):void{
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+    if (file) {
+      console.log(file);
+
+      this.storageRef.child('images/jjda-somethignelse.jpg')
+      .put(file).then(function(snapshot) {
+        console.log(snapshot);
+        console.log('Uploaded a blob or file!');
+      });;
+    }
+    console.log('submit');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgFkWRUS9smPFsLPP806XxToup18nzRBM",
+  authDomain: "da-review.firebaseapp.com",
+  databaseURL: "https://da-review.firebaseio.com",
+  storageBucket: "da-review.appspot.com",
+  messagingSenderId: "177689108109"
+};
+
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
