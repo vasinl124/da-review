90850ac858847dfba8c259bc83ff6e8e7d5bf3d9
Vasin Max Limsukhawat <max.limsukhawat@qsc.com>
2016-11-13 22:08:45 -0700

    scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Angulartics2, Angulartics2GoogleAnalytics } from 'angulartics2';
 import { Component, ViewContainerRef } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,22 @@ import { Component, ViewContainerRef } from '@angular/core';
 export class AppComponent {
   private viewContainerRef: ViewContainerRef;
 
-  public constructor(viewContainerRef:ViewContainerRef, angulartics2: Angulartics2, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
+  public constructor(viewContainerRef:ViewContainerRef, angulartics2: Angulartics2, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics, router: Router) {
     // You need this small hack in order to catch application root view container ref
     this.viewContainerRef = viewContainerRef;
+
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          // you can use DomAdapter
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(element); }
+        }
+      }
+      document.body.scrollTop = 0;
+    });
+
   }
 
   public isCollapsed:boolean = true;
